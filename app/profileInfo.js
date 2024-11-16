4d452319b7711dc8d10b00a7e0f831d2d6420920
thaibhoang <hoangthai.ftu53@gmail.com>
2024-11-16 17:51:33 +0700

    setup the profile

---
+++ app/profileInfo.js
@@ -0,0 +1,8 @@
+const profile = {
+  avatar:
+    "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Y3V0ZSUyMGNhdHxlbnwwfHwwfHx8MA%3D%3D",
+  name: "Super cute cat",
+  infor: "I like to nap, like a lot",
+};
+
+export default profile;
